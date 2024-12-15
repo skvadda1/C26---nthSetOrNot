3f6ef7bd4d71055010ce7799d9fd8d050dbd39e7
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-12-15 10:01:08 -0500

    Add files via upload

---
+++ nthBitSetorNot.py
@@ -0,0 +1,10 @@
+def setorNot(number,n):
+    mask = 1
+    if (n & mask) == 1 or (n & mask) == 0:
+        if number & (1 << (n-1)):
+            print("SET")
+        else:
+            print("NOT SET")
+number = int(input("Enter your number:"))
+n = int(input("Enter Bit Position"))
+setorNot(number,n)
